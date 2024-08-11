8ccc49a48a5768c98dc587d8a251a6d8e7d487dc
chikotaro <140727421+chikotaro@users.noreply.github.com>
2024-08-11 14:09:35 +0900

    🎉 進めてみた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import type { NextPage } from "next";
+import { About } from "src/component/About";
+import { Layout } from "src/component/Layout";
+import { MV } from "src/component/MV";
+import { Works } from "src/component/Works";
 
 const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+	return (
+		<Layout>
+			<div className="w-full">
+				<MV />
+				<div className="mx-auto mt-4 sm:max-w-[800px]">
+					<About />
+				</div>
+				<div className="mx-auto mt-20 sm:max-w-[800px]">
+					<Works />
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export default Home;
