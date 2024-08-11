c69dc9c8e794bfa5b37b9f505c5b7903d080621e
chikotaro <140727421+chikotaro@users.noreply.github.com>
2024-08-11 14:04:30 +0900

    ✨ cssの強化

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"light-gray": "#f5f5f5",
+				"dark-gray": "#d1d1d1",
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/forms"), require("tailwindcss-filters")],
 };
