27b36617d0ecde9faf7be014653f1c5194883dcb
chikotaro <140727421+chikotaro@users.noreply.github.com>
2024-08-17 23:17:56 +0900

    📝　情報の追加

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,14 +2,91 @@ import React from "react";
 import { Header } from "src/component/Header";
 import { Layout } from "src/component/Layout";
 import { Hero } from "src/Hero";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormInput {
+	name: string;
+	email: string;
+	content: string;
+}
 
 const Contact = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IFormInput>();
+	const onSubmit: SubmitHandler<IFormInput> = (data) => {
+		console.log(data);
+	};
+
 	return (
 		<div>
 			<Header />
 			<Hero title="お問い合わせ" subTitle="Contact" />
 			<Layout>
-				<h1>Contact Page</h1>
+				<h1 className="text-xl font-bold">お問い合わせフォーム</h1>
+
+				<form onSubmit={handleSubmit(onSubmit)} className="mt-10 space-y-6">
+					<div>
+						<label
+							htmlFor="name"
+							className="block text-sm font-medium text-gray-700"
+						>
+							名前
+						</label>
+						<input
+							id="name"
+							{...register("name", { required: "名前は必須です" })}
+							className="mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						/>
+						{errors.name ? (
+							<span className="text-red-600">{errors.name.message}</span>
+						) : null}
+					</div>
+					<div>
+						<label
+							htmlFor="email"
+							className="block text-sm font-medium text-gray-700"
+						>
+							メールアドレス
+						</label>
+						<input
+							id="email"
+							type="email"
+							{...register("email", { required: "メールアドレスは必須です" })}
+							className="mt-1 block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						/>
+						{errors.email ? (
+							<span className="text-red-600">{errors.email.message}</span>
+						) : null}
+					</div>
+					<div>
+						<label
+							htmlFor="content"
+							className="block text-sm font-medium text-gray-700"
+						>
+							お問い合わせ内容
+						</label>
+						<textarea
+							id="content"
+							{...register("content", {
+								required: "お問い合わせ内容は必須です",
+							})}
+							className="mt-1 block h-[250px] w-full rounded-md border border-gray-300 p-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						/>
+						{errors.content ? (
+							<span className="text-red-600">{errors.content.message}</span>
+						) : null}
+					</div>
+					<button
+						type="submit"
+						className="w-1/2 rounded-md bg-indigo-600 py-2 px-4 text-white hover:bg-indigo-700"
+					>
+						送信する
+					</button>
+				</form>
+
 				{/* 他のコンテンツ */}
 			</Layout>
 		</div>
--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,7 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { Header } from "src/component/Header";
 import { Layout } from "src/component/Layout";
 import { Hero } from "src/Hero";
+
+const workListA = [
+	{
+		title: "OHA!",
+		desc: "html,css,Javascript で作ったポートフォリオサイト",
+		href: "https://oha-portfolio-sho.vercel.app/",
+		thumbnail: "/OHA.png",
+	},
+	{
+		title: "AI",
+		desc: "html,css,Javascript で作ったポートフォリオサイト",
+		href: "https://ai-portfolio-sho.vercel.app/",
+		thumbnail: "/AI.png",
+	},
+];
+
+const workListB = [
+	{
+		title: "simple",
+		desc: "microCMS,Next.js,Typescriptで作ったポートフォリオサイト",
+		href: "https://my-next-project-q726eg5ph-shos-projects-c687e542.vercel.app",
+		thumbnail: "/simple.png",
+	},
+	{
+		title: "web-blog",
+		desc: "microCMS,Next.js,Typescriptで作ったポートフォリオサイト",
+		href: "https://pf-web-blog.vercel.app/",
+		thumbnail: "/web-blog.png",
+	},
+];
 
 const Works = () => {
 	return (
@@ -9,7 +41,61 @@ const Works = () => {
 			<Header />
 			<Hero title="実績紹介" subTitle="Works" />
 			<Layout>
-				<h1>Works Page</h1>
+				<p>これまで作成した制作物の一覧をご紹介します。</p>
+				<div className="mt-20">
+					<p className="text-xl sm:text-3xl">HTML,CSS,JavaScriptを使用した制作物</p>
+					<ul className="mt-5 flex flex-col gap-5 sm:grid sm:grid-cols-2">
+						{workListA.map((work) => (
+							<li key={work.href}>
+								<Link
+									href={work.href}
+									passHref
+									target="_blank"
+									className="flex transform flex-col gap-2 rounded-md border-2 p-5 transition-transform duration-300 hover:-translate-y-2 hover:shadow-lg sm:gap-4"
+								>
+									<div className="text-4xl font-bold">{work.title}</div>
+									<div>{work.desc}</div>
+									<div className="mx-auto w-[300px] pb-4">
+										<Image
+											src={work.thumbnail}
+											alt=""
+											width={300}
+											height={300}
+										/>
+									</div>
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+
+				<div className="mt-20">
+					<p className="text-xl sm:text-3xl">Next.jsを使用した制作物</p>
+					<ul className="mt-5 flex flex-col gap-5 sm:grid sm:grid-cols-2">
+						{workListB.map((work) => (
+							<li key={work.href}>
+								<Link
+									href={work.href}
+									passHref
+									target="_blank"
+									className="flex transform flex-col gap-2 rounded-md border-2 p-5 transition-transform duration-300 hover:-translate-y-2 hover:shadow-lg sm:gap-4"
+								>
+									<div className="text-4xl font-bold">{work.title}</div>
+									<div>{work.desc}</div>
+									<div className="mx-auto w-[300px] pb-4">
+										<Image
+											src={work.thumbnail}
+											alt=""
+											width={300}
+											height={300}
+										/>
+									</div>
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+
 				{/* 他のコンテンツ */}
 			</Layout>
 		</div>
